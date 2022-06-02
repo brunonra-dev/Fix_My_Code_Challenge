7a3c50027d1f2b792532625e967346c92cf45228
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2022-06-02 15:53:14 -0300

    fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
+"""
+squeareee
+"""
 
-class square():
-    
+
+class Square():
+    """ clas square"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ init method """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.height * self.width
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """ perimeter of square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ str formatt"""
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+if __name__ == "__main__":
+    """ call all function"""
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
